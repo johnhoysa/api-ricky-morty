6be09a9b92e9958d759ac51fbd91c58571240aef
johnHoysa <john.hoysa@gmail.com>
2025-07-16 20:59:35 -0500

    animate cards in

--- src/js/fetch-characters.js
+++ src/js/fetch-characters.js
@@ -1,5 +1,11 @@
-// get family members using API
+// GSAP Imports
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+// Register plugins here
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
+// get family members using API
 const getData = document.getElementById('letsStart');
 const appContainer = document.getElementById('app');
 
@@ -25,19 +31,45 @@ getData.addEventListener('click', () => {
     .then((family) => {
       const container = document.getElementById('familyCards');
       // Create card for each member
-      family.forEach((member) => {
+      family.forEach((member, index) => {
         const card = document.createElement('div');
+
+        // once data is recived scroll user to the section
+        gsap.to(window, {
+          duration: 0.5,
+          scrollTo: '#app',
+          offsetY: 152,
+          ease: 'power2.inOut'
+        });
+
         //card.className = 'card';
         card.classList.add('bg-white', 'rounded-md', 'p-4', 'cursor-pointer');
         card.innerHTML = `
             <img class="w-full rounded-md" src="${member.image}" alt="${member.name}">
             <h3 class="mt-2 text-center text-base">${member.name}</h3>
           `;
+
+        // Animate cards in on load
+        gsap.from(card, {
+          duration: 1, // Animation duration
+          y: 50, // Move 50px down
+          opacity: 0, // Fade in
+          delay: index * 0.2, // Stagger delay based on index
+          ease: 'power2.out' // Easing function
+        });
         // On card click
         card.addEventListener('click', () => {
           const firstName = member.name.split(' ')[0];
           // pass member name to step 2.
           showCharactersByFirstName(firstName);
+          // SCroll to next section
+          gsap.to(window, {
+            duration: 0.5,
+            scrollTo: '#results',
+            offsetY: 152,
+            delay: 1,
+            ease: 'power2.inOut'
+          });
         });
 
         container.appendChild(card);
@@ -58,7 +90,7 @@ getData.addEventListener('click', () => {
         const list = document.getElementById('relatedList');
         list.innerHTML = '';
 
-        data.results.forEach((char) => {
+        data.results.forEach((char, index) => {
           const card = document.createElement('div');
           card.classList.add(
             'bg-white',
@@ -74,6 +106,14 @@ getData.addEventListener('click', () => {
               <p class="text-sm">${char.species}</p>
             `;
           list.appendChild(card);
+          // Animate in
+          gsap.from(card, {
+            duration: 1, // Animation duration
+            y: 50, // Move 50px down
+            opacity: 0, // Fade in
+            delay: index * 0.2, // Stagger delay based on index
+            ease: 'power2.out' // Easing function
+          });
         });
       })
       .catch(() => {
