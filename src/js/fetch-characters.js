1660a75e4bac590e1ba54ee6ee941b6f983243e8
johnHoysa <john.hoysa@gmail.com>
2025-07-30 09:15:32 -0500

    updated Loading to white so could be seen

--- src/js/fetch-characters.js
+++ src/js/fetch-characters.js
@@ -104,12 +104,12 @@ getData.addEventListener('click', () => {
       results.innerHTML = `
   <h2 class="text-center mt-20 text-2xl text-white w-full lg:w-3/5 mx-auto">
   Okay, so, get this, multiverse stuff. Which version of me, I mean ${firstName} is, uh, your favorite? No wrong answers! Except maybe some.</h2>
-  <div class="flex flex-wrap gap-6 justify-center mt-8" id="relatedList" aria-live="polite">Loading...</div>`;
+  <div class="flex flex-wrap gap-6 justify-center mt-8 text-white" id="relatedList" aria-live="polite">Loading...</div>`;
     } else {
       results.innerHTML = `
   <h2 class="text-center pt-32 text-2xl text-white w-full lg:w-3/5 mx-auto">
   Okay, so, get this, multiverse stuff. Which version of the ${firstName} is, uh, your favorite? No wrong answers! Except maybe some.</h2>
-  <div class="flex flex-wrap gap-6 justify-center mt-8" id="relatedList" aria-live="polite">Loading...</div>`;
+  <div class="flex text-white flex-wrap gap-6 justify-center mt-8" id="relatedList" aria-live="polite">Loading...</div>`;
     }
 
     // lets fetch content related to the first name
@@ -331,7 +331,7 @@ function fadeOutOtherRelatedCards(clickedCardInner) {
       // ACTIVE CARD
       card.classList.add('clicked-on');
       gsap.to(card, {
-        y: 80,
+        y: 108,
         scale: 1.2,
         duration: 0.7,
         ease: 'power1.out',
